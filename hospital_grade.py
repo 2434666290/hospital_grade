550a76dd2070ca7af93272fead23db1f3ee5ea6d
2434666290 <131752374+2434666290@users.noreply.github.com>
2023-08-02 16:06:48 +0800

    Update hospital_grade.py

--- hospital_grade.py
+++ hospital_grade.py
@@ -30,29 +30,30 @@ def main():
 
         # 假设要读取的列名为'医院名称'，替换成你实际需要读取的列名
         column_name = '医院名称'
-        # 读取指定列的数据，进行查询并更新等级
-        for i in range(len(df)):
-            title = df.loc[i, column_name]
-            grade = hospital(title)
-            st.write("医院：", title)
-            st.write("等级：", grade)
-            if grade:
-                df.loc[i, '医院等级'] = grade
-            time.sleep(3)
-        # 显示DataFrame内容
-        st.write(df)
-        # 保存更新后的DataFrame到临时文件
-        tmp_file_path = os.path.join(os.getcwd(), 'tmp_data.xlsx')
-        df.to_excel(tmp_file_path, index=False)
-        # 将临时文件转换为Base64编码
-        with open(tmp_file_path, 'rb') as file:
-            base64_data = base64.b64encode(file.read()).decode()
-        # 以Markdown形式提供下载链接
-        st.markdown(
-            f"### [点击下载更新后的Excel文件](data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{base64_data})")
-        # 删除临时文件
-        os.remove(tmp_file_path)
-        st.success("Excel文件已更新！")
+        if st.button('开始爬取'): 
+             # 读取指定列的数据，进行查询并更新等级
+             for i in range(len(df)):
+                 title = df.loc[i, column_name]
+                 grade = hospital(title)
+                 st.write("医院：", title)
+                 st.write("等级：", grade)
+                 if grade:
+                     df.loc[i, '医院等级'] = grade
+                 time.sleep(3)
+             # 显示DataFrame内容
+             st.write(df)
+             # 保存更新后的DataFrame到临时文件
+             tmp_file_path = os.path.join(os.getcwd(), 'tmp_data.xlsx')
+             df.to_excel(tmp_file_path, index=False)
+             # 将临时文件转换为Base64编码
+             with open(tmp_file_path, 'rb') as file:
+                 base64_data = base64.b64encode(file.read()).decode()
+             # 以Markdown形式提供下载链接
+             st.markdown(
+                 f"### [点击下载更新后的Excel文件](data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{base64_data})")
+             # 删除临时文件
+             os.remove(tmp_file_path)
+             st.success("Excel文件已更新！")
 
 if __name__ == '__main__':
     main()
