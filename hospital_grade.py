de3be5ae2ba8f8a73535006d88e5d2b8beb6df2e
2434666290 <131752374+2434666290@users.noreply.github.com>
2023-08-02 12:07:32 +0800

    Update hospital_grade.py

--- hospital_grade.py
+++ hospital_grade.py
@@ -5,6 +5,25 @@ import time
 import streamlit as st
 import os
 import base64
+import re
+
+def hospital_name(title):
+    url_1 = 'https://www.qcc.com/web/search?key={title}'.format(title=title)
+    headers = {
+        'Cookie': 'qcc_did=aab8e627-253f-494b-b897-8a2a59d578f1; UM_distinctid=1898fb712c1431-04dd3c54ec68da-7c546c7d-e1000-1898fb712c21eb9; acw_tc=0ed7aca716909458999337914e1ec45cfad584452c29b677f1f984e5fa; QCCSESSID=91f457968a84d4a6fbd2183521; MQCCSESSID=bf13365a3d3beaafdf01260f12; CNZZDATA1254842228=515979886-1690330988-https%253A%252F%252Fwww.bing.com%252F%7C1690946655',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188'
+    }
+    response = requests.get(url_1, headers=headers)
+    # pass_word = urllib.parse.unquote(response.text)
+    html = etree.HTML(response.text)
+    hospital_name = html.xpath('//div[@class = "cname"]/a/text()')[0]
+    result = re.search(r'(.+?)\s*（', hospital_name)
+    if result:
+        extracted_content = result.group(1)
+        return extracted_content
+    else:
+        return hospital_name
+     
 def hospital(hospital_name):
     url = 'https://zgylbx.com/index.php?m=content&c=index&a=lists&catid=106&steps=&search=1&pc_hash=&k1=0&k2=0&k3=0&title={hospital_name}'.format(hospital_name=hospital_name)
     headers = {
@@ -31,6 +50,7 @@ def main():
         # 读取指定列的数据，进行查询并更新等级
         for i in range(len(df)):
             title = df.loc[i, column_name]
+            title = hospital_name(title)
             grade = hospital(title)
             st.write("医院：", title)
             st.write("等级：", grade)
